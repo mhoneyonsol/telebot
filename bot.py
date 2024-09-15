588549060ffd75da2654ace18d1a5760498e138e
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-09-15 11:44:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ You're embarking on an exciting adventure on the TON blockchain! 🚀
     keyboard = [
         [InlineKeyboardButton("🎨 Pixel MAP", url='https://t.me/pxltonbot/buypixels')],
         [InlineKeyboardButton("⚔️ Battle Pixel Art", url='https://t.me/pxltonbot/artbattle')],
-        [InlineKeyboardButton("⚔️ Farming", url='https://t.me/pxltonbot/farming')],
+        [InlineKeyboardButton("👩‍🌾 Farming", url='https://t.me/pxltonbot/farming')],
         [InlineKeyboardButton("👤 Profil", callback_data='profile')],
         [InlineKeyboardButton("🎁 Rewards", callback_data='rewards')],
         [InlineKeyboardButton("🏆 Leaderboard", callback_data='leaderboard')],
